03ef677dfd40747a0186caa6b7eb4744f9d2f12b
Yorvin Rosario <yorvin.rosario@axa-equitable.com>
2019-04-04 15:55:21 -0400

    added fake delete btn and counter

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,14 +1,43 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-  render() { 
+  
+  state = {
+    value: this.props.value
+  };
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.value === 0 ? "warning" : "primary";
+    return classes;
+  };
+
+  formatCount() {
+    return this.state.value === 0 ? "Zero" : this.state.value;
+  };
+
+  handleIncrement = () => {
+    this.setState({ value: this.state.value + 1 })
+  };
+
+  render() {
     return (
-      <React.Fragment>
-        <h1>Hello World</h1>
-        <button>Increment</button>
-      </React.Fragment>
+      <div>
+
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm "
+        >
+          Increment
+        </button>
+
+        <button onClick={() => this.props.onDelete(this.props.id) } className="btn btn-danger btn-sm m-2">Delete</button>
+
+      </div>
     );
-  }
+  };
 }
  
 export default Counter;
---
+++ src/components/counters.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import Counter from './counter';
+
+class Counters extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 0 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
+      { id: 5, value: 0 }
+    ]
+  };
+  
+  handleDelete = (counterId) => {
+    let counters = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({counters: counters})
+  };
+
+
+  render() {
+    return (
+      <div>
+        {this.state.counters.map(counter => (
+          <Counter
+            key={counter.id}
+            id={counter.id}
+            onDelete={this.handleDelete}
+            value={counter.value}
+          />
+        ))}
+      </div>
+    );
+  };
+}
+ 
+export default Counters;
