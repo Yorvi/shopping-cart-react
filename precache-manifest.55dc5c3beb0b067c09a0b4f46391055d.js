e76fd4bdd95273bf808d0402d6992bb6da173481
Yorvin Rosario <yorvin.rosario@axa-equitable.com>
2019-04-05 11:28:13 -0400

    Updates

---
+++ precache-manifest.55dc5c3beb0b067c09a0b4f46391055d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "218f8eb1b92a1be609cd",
+    "url": "/shopping-cart-react/static/js/runtime~main.36ec2802.js"
+  },
+  {
+    "revision": "777e64e14403197e5bad",
+    "url": "/shopping-cart-react/static/js/main.66ade08a.chunk.js"
+  },
+  {
+    "revision": "9b500fcb362f61c23fb8",
+    "url": "/shopping-cart-react/static/js/2.5b44faae.chunk.js"
+  },
+  {
+    "revision": "777e64e14403197e5bad",
+    "url": "/shopping-cart-react/static/css/main.a766df2a.chunk.css"
+  },
+  {
+    "revision": "9b500fcb362f61c23fb8",
+    "url": "/shopping-cart-react/static/css/2.73d5a7ad.chunk.css"
+  },
+  {
+    "revision": "b65e45e270afa64f6cdbaf426f4317d5",
+    "url": "/shopping-cart-react/index.html"
+  }
+];
